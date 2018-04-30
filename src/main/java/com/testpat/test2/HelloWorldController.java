751c42c70d54d04feb62aefc46f7fef8f9eb420e
patrice rolland <patricerolland@yahoo.fr>
2018-04-30 17:01:00 +0200

    add json webservice example

--- src/main/java/com/testpat/test2/HelloWorldController.java
+++ src/main/java/com/testpat/test2/HelloWorldController.java
@@ -1,8 +1,16 @@
 package com.testpat.test2;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class HelloWorldController {
@@ -14,10 +22,26 @@ public class HelloWorldController {
         return "Hello "+ name +"!";
     }
 
-   /* @GetMapping("/")
-    public String home() {
-        return "home";
-    }*/
+    @RequestMapping("/ajax")
+    public ModelAndView ajax() {
+        return new ModelAndView("ajax");
+    }
+
+    @RequestMapping(value = "/ajaxquery", method = RequestMethod.GET)
+    public @ResponseBody String getGreeting(@RequestParam String name) {
+        String result = "Hello! " + name + ". Time for now is " + new Date().toString();
+        bobo mybobo = new bobo("time",result);
+
+        ObjectMapper mapper =new ObjectMapper();
+        String jsonInString = null;
+        try {
+            jsonInString = mapper.writeValueAsString(mybobo);
+        } catch (JsonProcessingException exc) {
+            exc.printStackTrace();
+        }
+
+        return jsonInString;
+    }
 
     @GetMapping("/")
     public String home(final ModelMap pModel) {
